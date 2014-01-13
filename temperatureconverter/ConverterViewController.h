5b273115e0cfb47ac5c83054b9b140c10c27f072
Joey Lin <jvlin@yahoo-inc.com>
2014-01-13 15:36:32 -0800

    adding textfield delegate

--- temperatureconverter/ConverterViewController.h
+++ temperatureconverter/ConverterViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ConverterViewController : UIViewController
+@interface ConverterViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- temperatureconverter/ConverterViewController.m
+++ temperatureconverter/ConverterViewController.m
@@ -9,9 +9,11 @@
 #import "ConverterViewController.h"
 
 @interface ConverterViewController ()
+
 @property (weak, nonatomic) IBOutlet UITextField *fahrenheitTextField;
 @property (weak, nonatomic) IBOutlet UITextField *celciusTextField;
 @property (weak, nonatomic) IBOutlet UIButton *convertButton;
+@property BOOL isCelcius;
 
 - (IBAction)convertButtonPressed:(id)sender;
 
@@ -25,6 +27,7 @@
     if (self) {
         // Custom initialization
         self.title = @"Temperature Converter";
+        self.isCelcius = NO;
     }
     return self;
 }
@@ -34,6 +37,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     //[self.convertButton addTarget:self action:@selector(convertButtonClicked:) forControlEvents:(UIControlEvents)UIControlEventTouchDown];
+    self.fahrenheitTextField.delegate = self;
+    self.celciusTextField.delegate = self;
 
 }
 
@@ -47,19 +52,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self updateTemperatures];
+    [textField resignFirstResponder];
+    return YES;
+}
+
+
 - (IBAction)convertButtonPressed:(id)sender {
     [self.view endEditing:YES];
     NSLog(@"convert button pressed");
     [self updateTemperatures];
 }
 
+- (IBAction)fahrenheitEditingChanged:(id)sender {
+    NSLog(@"fahrenheit editing changed");
+    self.isCelcius = NO;
+}
+
+- (IBAction)celciusEditingChanged:(id)sender {
+    NSLog(@"celcius editing changed");
+    self.isCelcius = YES;
+}
+
+
 - (void)updateTemperatures {
-    //if fahrenheit given, calcuate celcius, else if only celcius is given, calculate farenheit
-    if ([self.fahrenheitTextField.text length] > 0) {
+    //convert the last edited text field
+    if (!self.isCelcius && [self.fahrenheitTextField.text length] > 0) {
         double fDegrees = [self.fahrenheitTextField.text doubleValue];
         double cDegrees = (fDegrees - 32.0) * (5.0/9.0);
         self.celciusTextField.text = [NSString stringWithFormat:@"%.0f", cDegrees];
-    } else if ([self.celciusTextField.text length] > 0) {
+    } else if (self.isCelcius && [self.celciusTextField.text length] > 0) {
         double cDegrees = [self.celciusTextField.text doubleValue];
         double fDegrees = (cDegrees * 9.0/5.0) + 32.0;
         self.fahrenheitTextField.text = [NSString stringWithFormat:@"%.0f", fDegrees];
